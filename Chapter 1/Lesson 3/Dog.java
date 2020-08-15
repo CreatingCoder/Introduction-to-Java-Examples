3e8241cfa10212dd5412b4134ce9a9d2b77fcd86
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-08-15 16:27:41 -0500

    Update Dog.java

--- Chapter 1/Lesson 3/Dog.java
+++ Chapter 1/Lesson 3/Dog.java
@@ -1,11 +1,11 @@
 class Dog {
 
-static void bark(){
-System.out.println("Bark");
-}
+   static void bark(){
+      System.out.println("Bark");
+   }
 
-public static void main(String args[]){
-bark();
-}
+   public static void main(String args[]){
+      bark();
+   }
 
 }
