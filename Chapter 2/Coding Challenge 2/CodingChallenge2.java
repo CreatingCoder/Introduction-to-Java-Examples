b34fb8de435674b9b0fee9c59a33d305376df241
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 16:39:46 -0500

    CodingChallenge2.java

---
+++ Chapter 2/Coding Challenge 2/CodingChallenge2.java
@@ -0,0 +1,16 @@
+class CodingChallenge2{
+
+   public static void main(String[] args){
+   
+      String str1 = "My favorite color starts with the letter: ";
+      String str2 = " and I am ";
+      String str3 = " years old.";
+   
+      char character = 'G';
+      int age = 18;
+   
+      System.out.println(str1 + character + str2 + age +str3);
+   
+   
+   }
+}
