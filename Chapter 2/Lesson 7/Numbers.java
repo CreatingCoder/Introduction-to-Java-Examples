4c406672f760e92ae0b75677e2484c387fe74c1c
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 16:35:44 -0500

    Numbers.java

---
+++ Chapter 2/Lesson 7/Numbers.java
@@ -0,0 +1,13 @@
+class Numbers{
+
+   public static void main(String[] args){
+   
+      int integer = 5;
+      double decimal = 2.25;
+   
+   
+      System.out.println(integer);
+      System.out.println(decimal);
+   
+   }
+}
