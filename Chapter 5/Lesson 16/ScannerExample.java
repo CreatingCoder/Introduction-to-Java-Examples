1d0605aad61022d29fd8d4762225b293656034ee
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 18:05:17 -0500

    ScannerExample.java

---
+++ Chapter 5/Lesson 16/ScannerExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ScannerExample {
+
+   public static void main(String[] args){
+   
+      Scanner scan = new Scanner(System.in);
+      
+      String userInput = scan.nextLine();
+      
+      System.out.println(userInput);
+      
+   }
+}
