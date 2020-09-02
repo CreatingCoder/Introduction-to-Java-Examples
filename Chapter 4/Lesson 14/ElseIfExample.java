1cf58cc52234b1bb829f3931e54a5c3eeb91ce52
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 17:38:35 -0500

    ElseIfExample.java

---
+++ Chapter 4/Lesson 14/ElseIfExample.java
@@ -0,0 +1,19 @@
+class ElseIfExample {
+
+   public static void main(String[] args){
+      int my_fav_num = 1;
+   
+      if(my_fav_num == 1){
+         System.out.println("One is the loneliest number.");
+      }
+      
+      else if(my_fav_num == 2){
+         System.out.println("Two can be as bad as one.");
+      }
+      
+      else{
+         System.out.println("Your favorite number isn't 1 or 2.");
+      }
+      
+   }
+}
