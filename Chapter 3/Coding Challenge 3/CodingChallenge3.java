9da9363cc0ea0e01b26875b86c19dfb9d4800acc
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 17:15:22 -0500

    CodingChallenge3.java

---
+++ Chapter 3/Coding Challenge 3/CodingChallenge3.java
@@ -0,0 +1,10 @@
+class CodingChallenge3 {
+
+   public static void main(String[] args){
+      int coffeePerDay = 2;
+      int daysInYear = 365;
+   
+      System.out.println(coffeePerDay * daysInYear);
+   
+   }
+}
