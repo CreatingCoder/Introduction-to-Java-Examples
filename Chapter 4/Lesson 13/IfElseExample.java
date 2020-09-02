e68149ef07f2bbccd38bfe126511802c5ef60a16
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 17:29:04 -0500

    IfElseExample.java

---
+++ Chapter 4/Lesson 13/IfElseExample.java
@@ -0,0 +1,15 @@
+class IfElseExample {
+
+   public static void main(String[] args){
+      Boolean hasCoffee = false;
+   
+      if(hasCoffee == true){
+         System.out.println("I am happy");
+      }
+      
+      else{
+         System.out.println("I am sad");
+      }
+      
+   }
+}
