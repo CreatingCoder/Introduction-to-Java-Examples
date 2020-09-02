f7cd20ba31d8bb477a28c8f8be6e62c6e642524e
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 18:55:07 -0500

    CodingChallenge5.java

---
+++ Chapter 5/Coding Challenge 5/CodingChallenge5.java
@@ -0,0 +1,15 @@
+class CodingChallenge5 {
+
+   public static void main(String[] args){
+   
+      String firstName[] = {"Harrison", "Clint", "Bruce"};
+      String lastName[] = {"Ford", "Clint", "Willis"};
+   
+      System.out.println("My favorite actor is " + firstName[0] + " " + lastName[0]);
+   
+      System.out.println("My 2nd favorite actor is " + firstName[1] + " " + lastName[1]);
+   
+      System.out.println("My 3rd favorite actor is " + firstName[2] + " " + lastName[2]);
+   
+   }
+}
