208099868a8145091ac2c38c8c161cf0613b7a02
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 17:49:48 -0500

    CommentsExample2.java

---
+++ Chapter 4/Lesson 15/CommentsExample2.java
@@ -0,0 +1,16 @@
+class CommentsExample2 {
+
+   public static void main(String[] args){
+   
+   //the 
+   //code 
+   //below 
+   //prints 
+   //a 
+   //line
+   
+      System.out.println("This code will only compile and execute" +
+         "when I have the comment above commented out");
+      
+   }
+}
