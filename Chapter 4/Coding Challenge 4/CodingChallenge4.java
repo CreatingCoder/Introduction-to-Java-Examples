c7bad3b35e3655337eff8aa1e9d8b023b6e847db
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 18:00:03 -0500

    CodingChallenge4.java

---
+++ Chapter 4/Coding Challenge 4/CodingChallenge4.java
@@ -0,0 +1,15 @@
+class CodingChallenge4 {
+
+   public static void main(String[] args){
+      Boolean I_like_sushi = false;
+   
+      if(I_like_sushi == true){
+         System.out.println("I could eat sushi everyday!");
+      }
+      
+      else if(I_like_sushi == false){
+         System.out.println("Raw fish? No thanks!");
+      }
+      
+   }
+}
