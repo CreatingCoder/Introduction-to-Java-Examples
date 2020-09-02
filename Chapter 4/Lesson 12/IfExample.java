677f912525593dfe6678547a3dc8e2f1e86cbc31
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 17:21:23 -0500

    IfExample.java

---
+++ Chapter 4/Lesson 12/IfExample.java
@@ -0,0 +1,11 @@
+class IfExample {
+
+   public static void main(String[] args){
+      Boolean hasCoffee = true;
+   
+      if(hasCoffee == true){
+         System.out.println("I am happy");
+      }
+      
+   }
+}
