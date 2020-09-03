777de09a5fc441d445bba8462d8532ac531c1b0d
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 21:43:22 -0500

    CodingChallenge7.java

---
+++ Chapter 7/Coding Challenge 7/CodingChallenge7.java
@@ -0,0 +1,13 @@
+class CodingChallenge7 {
+
+   public static void main(String[] args){
+      int time = 1;
+   
+      System.out.println("We have takeoff in ");
+   
+      for(int i = 10; i >= time; i--){
+         System.out.println(i);
+      }
+      System.out.println("Houston, we have takeoff.");
+   }
+}
