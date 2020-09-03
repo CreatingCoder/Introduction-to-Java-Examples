0e331893921d2ad999782acbedc50418cb4a0a35
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 22:16:35 -0500

    MethodParameterExample2.java

---
+++ Chapter 8/Lesson 31/MethodParameterExample2.java
@@ -0,0 +1,10 @@
+public class MethodParameterExample2 {
+
+   static void printingMethod(int num, String str){
+      System.out.println(num + " cups of " + str);
+   }
+
+   public static void main(String[] args){
+      printingMethod(2, "Coffee");   
+   }
+}
