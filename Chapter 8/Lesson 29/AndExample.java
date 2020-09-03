90e5d746fb05f55fdbd490b19285b6e6b4728ac8
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 21:55:38 -0500

    AndExample.java

---
+++ Chapter 8/Lesson 29/AndExample.java
@@ -0,0 +1,12 @@
+class AndExample {
+
+   public static void main(String[] args){
+      boolean likesCoffee = true;
+      boolean cupEmpty = true;
+   
+      if(likesCoffee == true && cupEmpty == true){
+         System.out.println("Want some more coffee?");
+      }
+      
+   }
+}
