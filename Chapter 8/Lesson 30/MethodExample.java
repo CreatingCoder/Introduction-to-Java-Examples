b87a6dd36727884a11072bd9fef238cae3800520
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 22:05:49 -0500

    MethodExample.java

---
+++ Chapter 8/Lesson 30/MethodExample.java
@@ -0,0 +1,10 @@
+public class MethodExample {
+
+   static void printingMethod(){
+      System.out.println("I am within the printingMethod");
+   }
+
+   public static void main(String[] args){
+      printingMethod();   
+   }
+}
