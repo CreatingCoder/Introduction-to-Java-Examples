d9a57864741c817a62dff0f70bac15941b9ce25e
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 21:59:10 -0500

    NotExample.java

---
+++ Chapter 8/Lesson 29/NotExample.java
@@ -0,0 +1,11 @@
+class NotExample {
+
+   public static void main(String[] args){
+      boolean hatesCoffee = false;
+   
+      if(!hatesCoffee){
+         System.out.println("I like coffee");
+      }
+      
+   }
+}
