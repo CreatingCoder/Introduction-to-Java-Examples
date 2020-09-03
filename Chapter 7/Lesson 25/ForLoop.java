634492401e99358d5ac74554080776a556af8e71
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 20:32:13 -0500

    ForLoop.java

---
+++ Chapter 7/Lesson 25/ForLoop.java
@@ -0,0 +1,13 @@
+class ForLoop {
+
+   public static void main(String[] args){
+      int count = 10;
+             
+      System.out.println("I am counting to " + count);         
+      
+      for(int i = 1; i<= count; i++){       
+         System.out.println(i);
+      }
+      
+   }
+}
