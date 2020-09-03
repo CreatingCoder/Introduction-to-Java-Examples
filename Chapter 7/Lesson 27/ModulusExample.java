62d9806b1b8511497e0897bc20680feea3cd78f6
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 20:49:09 -0500

    ModulusExample.java

---
+++ Chapter 7/Lesson 27/ModulusExample.java
@@ -0,0 +1,12 @@
+class ModulusExample {
+
+   public static void main(String[] args){
+   
+   //Example 1
+      System.out.println(9 % 4);
+   
+   //Example 2
+      System.out.println(9 % 3);
+   
+   }
+}
