ce1425b71d071def0c10f47cbab9fefeaf9f7a0f
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 22:14:51 -0500

    MethodParameterExample1.java

---
+++ Chapter 8/Lesson 31/MethodParameterExample1.java
@@ -0,0 +1,10 @@
+public class MethodParameterExample1 {
+
+   static void printingMethod(String str){
+      System.out.println(str + " is a parameter");
+   }
+
+   public static void main(String[] args){
+      printingMethod("Coffee");   
+   }
+}
