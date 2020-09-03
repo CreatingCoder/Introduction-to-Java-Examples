552ff6bd5cf4868403171cb70d27b9f6698df430
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 19:00:33 -0500

    EqualityOperator.java

---
+++ Chapter 6/Lesson 20/EqualityOperator.java
@@ -0,0 +1,15 @@
+class EqualityOperator {
+
+   public static void main(String[] args){
+      boolean codingIsFun = true;
+   
+      if(codingIsFun == true){
+         System.out.println("Coding is fun!");
+      }
+      
+      else{
+         System.out.println("Coding is not fun :(");
+      }
+   
+   }
+}
