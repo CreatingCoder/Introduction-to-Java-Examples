7da6ef6871f4eeba4e0300ea36db0e387595e217
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 19:07:57 -0500

    IncrementOperator.java

---
+++ Chapter 6/Lesson 21/IncrementOperator.java
@@ -0,0 +1,14 @@
+class IncrementOperator {
+
+   public static void main(String[] args){
+      boolean isMarried = true;
+      int numInHousehold = 1;
+   
+      if(isMarried == true){
+         numInHousehold++;
+      }
+      
+      System.out.println(numInHousehold);
+   
+   }
+}
