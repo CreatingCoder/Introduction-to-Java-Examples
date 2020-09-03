a1bb913e779051186539aec86109960a79f9c06a
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 19:10:05 -0500

    DecrementOperator.java

---
+++ Chapter 6/Lesson 21/DecrementOperator.java
@@ -0,0 +1,14 @@
+class DecrementOperator {
+
+   public static void main(String[] args){
+      boolean isFull = false;
+      int numOfSnickers = 1;
+   
+      if(isFull != true){
+         numOfSnickers--;
+      }
+      
+      System.out.println(numOfSnickers);
+   
+   }
+}
