9784813c3359c3db610ce3ed539bd32295dc2ffb
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 19:03:54 -0500

    RelationalOperator.java

---
+++ Chapter 6/Lesson 20/RelationalOperator.java
@@ -0,0 +1,16 @@
+class RelationalOperator {
+
+   public static void main(String[] args){
+      int age = 26;
+      int ageLimit = 21;
+   
+      if(age >= ageLimit){
+         System.out.println("Let's have a beer");
+      }
+      
+      else{
+         System.out.println("I'll have a Coke");
+      }
+   
+   }
+}
