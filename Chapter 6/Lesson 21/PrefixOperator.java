67585a61b2d9d03f737300f107657a87c06ef8dd
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 19:14:50 -0500

    PrefixOperator.java

---
+++ Chapter 6/Lesson 21/PrefixOperator.java
@@ -0,0 +1,11 @@
+class DecrementOperator {
+
+   public static void main(String[] args){
+      int i = 10;
+      int j = ++i * 2;
+   
+      System.out.println("i = " + i);
+      System.out.println("j = " + j);
+   
+   }
+}
