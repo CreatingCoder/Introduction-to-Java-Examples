c03204b34429cb0e6b41df74f7b833dfb115c28f
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-02 21:57:42 -0500

    OrExample.java

---
+++ Chapter 8/Lesson 29/OrExample.java
@@ -0,0 +1,12 @@
+class OrExample {
+
+   public static void main(String[] args){
+      boolean likesCoffee = true;
+      boolean hotTemp = false;
+   
+      if(likesCoffee == true || cupEmpty == true){
+         System.out.println("An iced coffee sounds good.");
+      }
+      
+   }
+}
