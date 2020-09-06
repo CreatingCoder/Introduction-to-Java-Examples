da46e5eccd6e40bb2a1422aa540a2af607c192e6
CreatingCoder <69217993+CreatingCoder@users.noreply.github.com>
2020-09-05 22:34:53 -0500

    PersonList.java

---
+++ Chapter 9/Lesson 32/PersonList.java
@@ -0,0 +1,12 @@
+public class PersonList {
+
+   public static void main(String[] args){
+   
+      Person Fireman = new Person("Fireman", 25);
+      
+      Person Soldier = new Person("Rifleman", 18);
+      
+      Person Doctor = new Person("Surgeon", 48);
+   
+   }
+}
